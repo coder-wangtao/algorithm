f2999f09ad20eea60e1b3ff6744106c84b8c0412
w t <1475929340@qq.com>
2025-08-11 17:35:05 +0800

    .

--- 第五天/3-K 个一组翻转链表.js
+++ 第五天/3-K 个一组翻转链表.js
@@ -17,21 +17,21 @@ head.next.next.next = new ListNode(4);
 head.next.next.next.next = new ListNode(5);
 
 var reverseKGroup = function (head, k) {
-  let pre = null;
-  let cur = head;
-  let p = head;
+  let pre = null; // 初始化 pre 为 null，用来保存已反转部分的前一个节点
+  let cur = head; // cur 指向当前节点，从 head 开始
+  let p = head; // p 用来遍历链表，检查是否有足够的节点进行反转
   for (let i = 0; i < k; i++) {
-    if (p == null) return head;
-    p = p.next;
+    if (p == null) return head; // 如果 p 遇到 null，说明剩余节点不足 k 个，不需要反转，直接返回原始 head
+    p = p.next; // p 向下移动，遍历到下一个节点
   }
   for (let j = 0; j < k; j++) {
-    let next = cur.next;
-    cur.next = pre;
-    pre = cur;
-    cur = next;
+    let next = cur.next; // 暂时保存 cur 的下一个节点
+    cur.next = pre; // 将 cur 的 next 指向 pre，从而完成反转
+    pre = cur; // 将 pre 更新为 cur，移动到下一个节点
+    cur = next; // 将 cur 移动到下一个节点
   }
-  head.next = reverseKGroup(cur, k);
-  return pre;
+  head.next = reverseKGroup(cur, k); // 递归调用反转剩余部分，将原链表剩余部分进行反转
+  return pre; // 返回当前反转部分的头节点，即新的头节点
 };
 
 // 输出原链表

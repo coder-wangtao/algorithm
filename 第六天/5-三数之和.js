1cdb6d0efca153e0789f7b0fc6d1400790c2fa36
w t <1475929340@qq.com>
2025-08-25 15:20:33 +0800

    .

--- 第六天/5-三数之和.js
+++ 第六天/5-三数之和.js
@@ -1,8 +1,7 @@
 // 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，
 // 同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
 const nums = [-1, 0, 1, 2, -1, -4];
-
-
+//[-4,-1,-1,0,1,2]
 var threeSum = function (nums) {
   nums = nums.sort((a, b) => a - b);
   let res = [];

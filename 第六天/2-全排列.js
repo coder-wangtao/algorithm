389be95fb2ca14abc2204938aacf9eb3a0127cf5
w t <1475929340@qq.com>
2025-08-25 14:00:09 +0800

    .

--- 第六天/2-全排列.js
+++ 第六天/2-全排列.js
@@ -1,31 +1,42 @@
 // 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
 // nums = [1,2,3]
 //[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
-
+//1 2,3
 function permute(nums) {
-  let res = [];
-  debugger;
-  perm(nums, 0, nums.length - 1, res);
+  if (!nums) {
+    return;
+  }
+  const res = [];
+
+  const backtarck = (path) => {
+    if (path.length === nums.length) {
+      res.push(path);
+      return;
+    }
+
+    nums.forEach((n) => {
+      if (path.includes(n)) return;
+      backtarck([...path, n]);
+    });
+  };
+  backtarck([]);
   return res;
 }
+// backtrack([])
+//   -> 1 not in path -> backtrack([1])
+//       -> 2 not in path -> backtrack([1,2])
+//           -> 3 not in path -> backtrack([1,2,3]) => push [1,2,3]
+//       -> 3 not in path -> backtrack([1,3])
+//           -> 2 not in path -> backtrack([1,3,2]) => push [1,3,2]
+//   -> 2 not in path -> backtrack([2])
+//       -> 1 not in path -> backtrack([2,1])
+//           -> 3 not in path -> backtrack([2,1,3]) => push [2,1,3]
+//       -> 3 not in path -> backtrack([2,3])
+//           -> 1 not in path -> backtrack([2,3,1]) => push [2,3,1]
+//   -> 3 not in path -> backtrack([3])
+//       -> 1 not in path -> backtrack([3,1])
+//           -> 2 not in path -> backtrack([3,1,2]) => push [3,1,2]
+//       -> 2 not in path -> backtrack([3,2])
+//           -> 1 not in path -> backtrack([3,2,1]) => push [3,2,1]
 
-// p 全排列的开始位置 q 全排列的结束位置
-function perm(arr, p, q, res) {
-  if (p === q) {
-    res.push([...arr]);
-  } else {
-    for (let i = p; i <= q; i++) {
-      swap(arr, i, p);
-      perm(arr, p + 1, q, res);
-      swap(arr, i, p);
-    }
-  }
-}
-
-function swap(arr, p, q) {
-  let temp = arr[p];
-  arr[p] = arr[q];
-  arr[q] = temp;
-}
-
-permute([1, 2]);
+console.log(permute([1, 2, 3]));

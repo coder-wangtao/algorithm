8801fbe60cac9c860056f5cc50352c4e842de50d
w t <1475929340@qq.com>
2025-09-02 11:03:57 +0800

    .

--- 第十天/4-最长公共子序列.js
+++ 第十天/4-最长公共子序列.js
@@ -24,11 +24,11 @@
 const text1 = "abcde";
 const text2 = "ace";
 var longestCommonSubsequence = function (text1, text2) {
-  let n = text1.length;
-  let m = text2.length;
-  let dp = Array.from(new Array(n + 1), () => new Array(m + 1).fill(0));
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= m; j++) {
+  let m = text1.length;
+  let n = text2.length;
+  let dp = Array.from(new Array(m + 1), () => new Array(n + 1).fill(0));
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
       if (text1[i - 1] == text2[j - 1]) {
         dp[i][j] = dp[i - 1][j - 1] + 1;
       } else {
@@ -36,7 +36,7 @@ var longestCommonSubsequence = function (text1, text2) {
       }
     }
   }
-  return dp[n][m];
+  return dp[m][n];
 };
 
 console.log(longestCommonSubsequence(text1, text2));

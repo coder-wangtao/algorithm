c04907bfdfc944419eb15bb7567a7e633ee8b274
w t <1475929340@qq.com>
2025-09-08 11:25:18 +0800

    .

--- 第十三天/4-重排链表.js
+++ 第十三天/4-重排链表.js
@@ -4,15 +4,40 @@
 // L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
 // 不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
 
-// 将链表每个节点断开，放入数组s
-// 指针i从头向尾，指针j从尾向头，重新组装链表
+// 1.将链表线性存入数组，置空指针。
+// 2.遍历数组重组链表，偶数栈顶弹出，奇数队列出队。
 
-var reorderList = function (head, s = [], tmp) {
-  // 换行可删除，合并到4行
-  while (head)
-    (tmp = head.next), (head.next = null), s.push(head), (head = tmp);
-  var i = -1,
-    j = s.length;
-  while (++i < --j) (s[i].next = s[j]), j !== i + 1 && (s[j].next = s[i + 1]);
-  return s[0];
-};
+// 创建一个测试链表：1 -> 2 -> 3 -> 4  ==> 1 -> 4 -> 2 -> 3
+
+class ListNode {
+  constructor(val = 0, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+const head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+
+function reorderList(head) {
+  const arr = [];
+
+  while (head) {
+    const temp = head.next;
+    head.next = null;
+    arr.push(head);
+    head = temp;
+  }
+  let cur = arr.shift(),
+    i = 0;
+  debugger;
+  while (arr.length) {
+    cur.next = i++ % 2 === 0 ? arr.pop() : arr.shift();
+    cur = cur.next;
+  }
+}
+
+reorderList(head);
+console.log(head);

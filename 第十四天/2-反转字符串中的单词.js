e214260d870d76b3fb44d67612141cc4ca2e352f
w t <1475929340@qq.com>
2025-09-09 10:39:47 +0800

    .

--- 第十四天/2-反转字符串中的单词.js
+++ 第十四天/2-反转字符串中的单词.js
@@ -1,1 +1,34 @@
-    
+// 给你一个字符串 s ，请你反转字符串中 单词 的顺序。
+// 单词 是由非空格字符组成的字符串。s 中使用至少一个空格将字符串中的 单词 分隔开。
+// 返回 单词 顺序颠倒且 单词 之间用单个空格连接的结果字符串。
+// 注意：输入字符串 s中可能会存在前导空格、尾随空格或者单词间的多个空格。返回的结果字符串中，单词间应当仅用单个空格分隔，且不包含任何额外的空格。
+
+const s = "the sky is blue";
+
+var reverseWords = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+  let queue = [];
+  let word = "";
+  //去除
+  while (s.charAt(left) === " ") {
+    left++;
+  }
+  while (s.charAt(right) === " ") {
+    right--;
+  }
+  while (left <= right) {
+    let char = s.charAt(left);
+    if (char === " " && word) {
+      queue.unshift(word);
+      word = "";
+    } else if (char !== " ") {
+      word += char;
+    }
+    left++;
+  }
+  queue.unshift(word);
+  return queue.join(" ");
+};
+
+console.log(reverseWords(s));

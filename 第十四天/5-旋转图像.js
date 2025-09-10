279ab955d6ff7b4b28043debc508e395aeff7519
w t <1475929340@qq.com>
2025-09-10 10:17:45 +0800

    .

--- 第十四天/5-旋转图像.js
+++ 第十四天/5-旋转图像.js
@@ -2,20 +2,29 @@
 
 // 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
 
+// 通过观察可以发现，旋转后的矩阵最后一列为原矩阵第一行，倒数第二列为原矩阵第二行...
+// 故，遍历外层数组，将该行的每一项依次出栈，并倒序压入每一列的队首，即可获得结果
+
 const matrix = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9],
 ];
 
+// const matrix = [
+//   [7, 4, 1],
+//   [8, 5, 2],
+//   [9, 6, 3],
+// ];
+
 var rotate = function (matrix) {
-  let length = matrix.length;
-  for (let i = 0; i < length; i++) {
-    for (let j = i; j < length; j++) {
-      let temp = matrix[i][j];
-      matrix[i][j] = matrix[j][i];
-      matrix[j][i] = temp;
+  const len = matrix.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      let curr = matrix[i].pop();
+      matrix[len - j - 1].unshift(curr);
     }
   }
-  return matrix.map((item) => item.reverse());
 };
+
+console.log(rotate(matrix));

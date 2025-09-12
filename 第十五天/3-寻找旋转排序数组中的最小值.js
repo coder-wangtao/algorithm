60054da278a278978f5c14a2e363a57cc7489073
w t <1475929340@qq.com>
2025-09-12 10:16:51 +0800

    .

--- 第十五天/3-寻找旋转排序数组中的最小值.js
+++ 第十五天/3-寻找旋转排序数组中的最小值.js
@@ -13,7 +13,6 @@ const nums = [3, 4, 5, 1, 2];
 var findMin = function (nums) {
   let start = 0;
   let end = nums.length - 1;
-  debugger;
   while (start < end) {
     const mid = Math.floor((start + end) / 2);
     if (nums[mid] > nums[end]) {
---
+++ 第十五天/4-删除排序链表中的重复元素 II.js
@@ -0,0 +1,50 @@
+//给定一个已排序的链表的头 head ， 删除原始链表中所有重复数字的节点，只留下不同的数字 。返回 已排序的链表 。
+
+// 1 -> 2 -> 3 -> 3 -> 4 -> 4 -> 5
+// 1 -> 2 -> 3
+
+class ListNode {
+  constructor(val = 0, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+const l1 = new ListNode(1);
+const l2 = new ListNode(2);
+const l3 = new ListNode(3);
+const l4 = new ListNode(3);
+const l5 = new ListNode(4);
+const l6 = new ListNode(4);
+const l7 = new ListNode(5);
+
+const head = l1;
+head.next = l2;
+head.next.next = l3;
+head.next.next.next = l4;
+head.next.next.next.next = l5;
+head.next.next.next.next.next = l6;
+head.next.next.next.next.next.next = l7;
+
+var deleteDuplicates = function (head) {
+  // 哑节点 next指向head
+  let dummy = new ListNode(0, head);
+  let cur = dummy;
+  while (cur.next && cur.next.next) {
+    // 相邻结点的值相同的情况(下一个节点的值等于下下一个节点的值)
+    if (cur.next.val == cur.next.next.val) {
+      // 先把相同的值保存起来，后面节点值等于此值的都从链表中删除
+      let val = cur.next.val;
+      // 直接删除后面链接中值等于这个值的节点
+      while (cur.next && cur.next.val == val) {
+        cur.next = cur.next.next;
+      }
+    } else {
+      // 相邻结点的值不相同的情况  直接遍历后一个
+      cur = cur.next;
+    }
+  }
+  return dummy.next;
+};
+
+console.log(deleteDuplicates(head));

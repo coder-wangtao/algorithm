e591e348c129204d8bee153e52cd2ac3253029dc
w t <1475929340@qq.com>
2025-09-17 10:53:48 +0800

    .

--- 第十六天/5-在排序数组中查找元素的第一个和最后一个位置.js
+++ 第十六天/5-在排序数组中查找元素的第一个和最后一个位置.js
@@ -4,35 +4,44 @@
 // 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
 const nums = [5, 7, 7, 8, 8, 10],
   target = 8;
+
+//有序数组查找目标值问题首先想到的是二分法，查找左边界和右边界需要用两次二分法。
+// 这里将二分查找放入一个函数中，函数返回的是左边界。
+// 两次调用这个函数，第一次获得target的左边界，第二次获得target + 1的左边界，将第二次的结果减一就是target的右边界。
+
 var searchRange = function (nums, target) {
-  const len = nums.length;
-  let l1 = 0;
-  let r1 = len - 1;
-  let l2 = 0;
-  let r2 = len - 1;
+  var left = 0;
+  var right = nums.length - 1;
+  var first = -1;
+  var last = -1;
 
-  while (l1 < r1 || l2 < r2) {
-    const mid1 = Math.floor((l1 + r1) / 2);
-    const mid2 = Math.ceil((l2 + r2) / 2);
-
-    if (target > nums[mid1]) {
-      l1 = mid1 + 1;
+  // 找第一个等于 target 的位置
+  while (left <= right) {
+    let mid = (left + right) >> 1;
+    if (nums[mid] === target) {
+      right = mid - 1;
+      first = mid;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
     } else {
-      r1 = mid1;
-    }
-
-    if (target < nums[mid2]) {
-      r2 = mid2 - 1;
-    } else {
-      l2 = mid2;
+      left = mid + 1;
     }
   }
 
-  if (nums[l1] !== target) {
-    return [-1, -1];
+  // 找最后一个等于 target 的位置
+  left = 0;
+  right = nums.length - 1;
+  while (left <= right) {
+    let mid = (left + right) >> 1;
+    if (nums[mid] === target) {
+      left = mid + 1;
+      last = mid;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
   }
-
-  return [l1, l2];
+  return [first, last];
 };
-
 console.log(searchRange(nums, target));

6223f505539bc6675e241b1018dbf6a66f4d9ea0
w t <1475929340@qq.com>
2025-10-10 18:02:00 +0800

    .

--- 第十天/1-翻转二叉树.js
+++ 第十天/1-翻转二叉树.js
@@ -1,5 +1,4 @@
-// 编写一个函数来查找字符串数组中的最长公共前缀。
-// 如果不存在公共前缀，返回空字符串 ""。
+// 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
 
 class TreeNode {
   constructor(val, left = null, right = null) {
@@ -9,6 +8,10 @@ class TreeNode {
   }
 }
 
+//  1
+//2  3
+//  1
+//3  2
 const root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
 
 var invertTree = function (root) {

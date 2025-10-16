b5350d9d2886fd099324d3af92547e8b304d2650
w t <1475929340@qq.com>
2025-10-16 19:41:12 +0800

    .

--- 第十六天/4-从中序与后序遍历序列构造二叉树.js
+++ 第十六天/4-从中序与后序遍历序列构造二叉树.js
@@ -1,6 +1,11 @@
 // 给定两个整数数组 inorder 和 postorder ，其中 inorder 是二叉树的中序遍历，
 //  postorder 是同一棵树的后序遍历，请你构造并返回这颗 二叉树 。
 
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.right = right === undefined ? null : right;
+}
+
 const inorder = [9, 3, 15, 20, 7];
 const postorder = [9, 15, 7, 20, 3];
 //     3

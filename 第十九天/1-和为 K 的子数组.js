88937e850767cfe5f0cb5258c942785ac2c9b5f3
w t <1475929340@qq.com>
2025-12-23 10:38:11 +0800

    .

--- 第十九天/1-和为 K 的子数组.js
+++ 第十九天/1-和为 K 的子数组.js
@@ -1,25 +1,37 @@
 // 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的子数组的个数 。
-
 // 子数组是数组中元素的连续非空序列。
+// 输入：nums = [1,2,3], k = 3
+// 输出：2
+// 使用前缀和 + 哈希表，记录每个前缀和出现的次数，通过查找 sum - k 来统计满足条件的连续子数组个数。
 
+// 前缀和：即数组该位置及之前的元素之和。
+// 举例：[2,1,3,4]，则前缀和为[0,2,3,6,10]，这里需要第一个元素为0。
+// 这题的思路是首先求得前缀和befores，before.length=nums.length+1；
+// 由于是要求数组中和为k的连续子数组的个数，可以联想到假设某个前缀和-k这个值也是某个前缀和，即存在的；
+// 由于数组中元素的范围是[-1000,1000]，故不同位置的前缀和可能相同，故使用Map这种结构保存各个前缀和的个数。
+
+//TODO:解析 https://www.bilibili.com/video/BV1gN411E7Zx/?spm_id_from=333.337.search-card.all.click
 const nums = [1, 2, 3],
   k = 3;
 var subarraySum = function (nums, k) {
-  let ans = 0;
-  let sum = 0;
+  // 用来存储 前缀和 及其出现的次数。
+  // 前缀和：从数组开头到当前位置的累加和。
   let map = new Map();
-  //对与单个为k的数来说，相当如减去前缀和为0的值等于k
-  map.set(0, 1); //这是非常有必要的
+  let sum = 0;
+  let count = 0;
+  map.set(0, 1); // 处理从数组开头就能形成目标和 k 的情况。
   for (let i = 0; i < nums.length; i++) {
     sum += nums[i];
+    // 如果之前出现过前缀和等于 sum - k
+    // 那么从那个前缀和后的元素到当前元素的子数组和就是 k
+    // 返回这样的前缀和出现次数，有多少个就加多少到 count
     if (map.has(sum - k)) {
-      ans += map.get(sum - k);
+      count += map.get(sum - k);
     }
-    //可能相同的前缀和出现多次,因为有负数，0之类的
-    map.set(sum, (map.get(sum) || 0) + 1);
+    //记录前缀和出现的次数
+    map.has(sum) ? map.set(sum, map.get(sum) + 1) : map.set(sum, 1);
   }
-
-  return ans;
+  return count;
 };
 
 console.log(subarraySum(nums, k));

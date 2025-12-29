c6246970af4bec82d8195f7ea79e5276ef5ac8ce
w t <1475929340@qq.com>
2025-12-29 10:42:27 +0800

    .

--- 第四天/2-链表中倒数第k个节点.js
+++ 第四天/2-链表中倒数第k个节点.js
@@ -1,3 +1,11 @@
+// 实现一种算法，找出单向链表中倒数第 k 个节点。返回该节点的值。
+// 注意：本题相对原题稍作改动
+// 示例：
+// 输入： 1->2->3->4->5 和 k = 2
+// 输出： 4
+// 说明：
+// 给定的 k 保证是有效的。
+
 class ListNode {
   constructor(val = 0, next = null) {
     this.val = val;
@@ -17,6 +25,7 @@ head.next.next = l3;
 head.next.next.next = l4;
 head.next.next.next.next = l5;
 //1->2->3->4->5
+//1 1
 var getKthFromEnd = function (head, k) {
   let fast = head;
   let slow = head;
